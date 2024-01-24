fd9bc548e77d7d8c19bdcbcd65a4b732b2147478
Victor Saraev <vvsar@mail.ru>
2024-01-24 04:09:28 +0300

    feat: add create.js solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import fs from 'node:fs/promises';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+function checkFileExists(file) {
+  return fs.access(file, fs.constants.F_OK)
+    .then(() => true)
+    .catch(() => false)
+}
+
 const create = async () => {
-    // Write your code here 
+  try {
+    if (await checkFileExists(`${__dirname}/files/fresh.txt`)) {
+      throw new Error('FS operation failed');
+    }
+    const content = 'I am fresh and young';
+    await fs.writeFile(`${__dirname}/files/fresh.txt`, content);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await create();
+
+// throw new Error('FS operation failed');
