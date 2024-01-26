7d06127481a206579c57a466b2d80b5b16ec1c35
Victor Saraev <vvsar@mail.ru>
2024-01-26 16:29:07 +0300

    feat: add modules solution

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,23 @@
 import path from 'path';
 import { release, version } from 'os';
 import { createServer as createServerHttp } from 'http';
-import './files/c';
+import './files/c.js';
+import * as unknownObject_a from './files/a.json' assert { type: "json" };
+import * as unknownObject_b from './files/b.json' assert { type: "json" };
+import { fileURLToPath } from 'url';
+import { assert } from 'console';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+  unknownObject = unknownObject_a;
 } else {
-    unknownObject = require('./files/b.json');
+  unknownObject = unknownObject_b;
 }
 
 console.log(`Release ${release()}`);
